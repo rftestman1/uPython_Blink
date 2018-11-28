20007bb09938b547eb8d9d6cbc62d013b2ba73ca
rftestman1 <sspaman1@gmail.com>
2018-11-28 16:06:14 -0500

    New repository 11/27/18

---
+++ main.py
@@ -0,0 +1,20 @@
+import utime
+from machine import Pin
+
+
+def main():
+    led = Pin(2, Pin.OUT)
+    enabled = False
+    while True:
+        if enabled:
+            led.off()
+            print('LED Off')
+        else:
+            led.on()
+            print('LED on')
+        utime.sleep_ms(1000)
+        enabled = not enabled
+
+
+if __name__ == '__main__':
+    main()
